1c4bdb74203e0db305cc54719f77a3b74b7b1dc6
Baptiste CHALEIL <bchaleil@e3r10p5.42.fr>
2016-03-15 19:46:07 +0100

    List dir

--- srcs/list_dir.c
+++ srcs/list_dir.c
@@ -1,28 +1,63 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   list_dir.c                                         :+:      :+:    :+:   */
+/*   list_file_info.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: bchaleil <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/07 15:53:44 by bchaleil          #+#    #+#             */
-/*   Updated: 2016/03/07 18:35:47 by bchaleil         ###   ########.fr       */
+/*   Updated: 2016/03/15 19:28:23 by bchaleil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
 
-void	list_dir(char *dir_name)
+static t_file	*new_elem(char *name, char *path, struct stat fs)
+{
+	t_file		*f;
+
+	if (!(f = ft_memalloc(sizeof(t_file))))
+		return (NULL);
+	f->name = name;
+	f->path = path;
+	f->fs = fs;
+	f->next = NULL;
+	return (f);
+}
+
+static t_file	*push_front(t_file *lst, t_file *el)
+{
+	if (lst == NULL)
+		return (el);
+	else
+		el->next = lst;
+	return (el);
+}
+
+t_file				*list_file_info(char *dir_name)
 {
 	struct dirent	*p_dirent;
 	DIR				*p_dir;
+	t_file			*list;
+	struct stat		fs;
+	char			*path;
 
+	list = NULL;
 	p_dir = opendir(dir_name);
 	if (p_dir == NULL)
 	{
-		return ls_perror(dir_name);
+		ls_perror(dir_name);
+		return (NULL);
 	}
 	while ((p_dirent = readdir(p_dir)) != NULL)
-		ft_putendl(p_dirent->d_name);
+	{
+		path = get_path(dir_name);
+		ft_putendl(path);
+		if (stat(ft_strjoin(path, p_dirent->d_name), &fs) == -1)    
+			ls_perror("stat fail");
+		else
+			list = push_front(list, new_elem(dir_name, path, fs));
+	}
 	closedir(p_dir);
+	return (list);
 }
--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: bchaleil <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/07 15:18:58 by bchaleil          #+#    #+#             */
-/*   Updated: 2016/03/11 19:21:46 by bchaleil         ###   ########.fr       */
+/*   Updated: 2016/03/15 19:28:26 by bchaleil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,48 +14,28 @@
 
 int	main(int ac, char **av)
 {
-	int				i;
-	struct stat		file_stat;
-	char			*linkname;
-	ssize_t			r;
 
-	i = 0;
-	while (++i < ac)
-		list_dir(av[i]);
-	if (lstat(av[1], &file_stat) < 0)    
-        return (1);
-
-	ft_putstr("\n\n");
-	print_mode(file_stat.st_mode);
-	print_attr(av[1]);
-	ft_putstr("  ");
-    ft_putnbr(file_stat.st_nlink);
-	ft_putstr("  ");
-	print_user(file_stat.st_uid);
-	ft_putstr("  ");
-	print_group(file_stat.st_gid);
-	ft_putstr("  ");
-	ft_putnbrll(file_stat.st_size);
-	ft_putstr(ctime(&file_stat.st_mtimespec.tv_sec));
-	ft_putstr("  ");
-	ft_putstr(av[1]);
-	if (S_ISLNK(file_stat.st_mode))
+	t_file	*f;
+	/*
+	t_flag	flag;
+	flag = argv_check(ac, av);
+	//debug
+	ft_putstr("flags: ");
+	ft_print_binary(flag);
+	ft_putstr("\n");
+	*/
+	if (ac == 1)
+		return (0);
+	f = list_file_info(av[1]);
+	while (f)
 	{
-		linkname = ft_strnew(file_stat.st_size);
-		r = readlink(av[1], linkname, file_stat.st_size + 1);
-		if (r == -1)
-		{
-			perror("readlink");
-			exit(EXIT_FAILURE);
-		}
-		ft_putstr("->");
-		ft_putendl(linkname);
-		ft_memdel((void**)&linkname);
+		ft_putstr("st mode ");
+		print_mode(f->fs.st_mode);
+		ft_putstr("\nf->name ");
+		ft_putendl(f->name);
+		ft_putstr("f->path ");
+		ft_putendl(f->path);
+		f = f->next;
 	}
-
-    printf("File Size: \t\t%lld bytes\n", file_stat.st_size);
-	//Used to count the total
-	//Count the invisible file only if the a option is provided
-    printf("File Size: \t\t%lld bytes\n", file_stat.st_blocks);
 	return (0);
 }
