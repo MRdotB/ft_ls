ec7f7ffda955a97f35024ff5b2c162d0bcf1e793
Baptiste CHALEIL <bchaleil@e2r9p10.42.fr>
2016-03-16 21:54:16 +0100

    Call func on main

--- srcs/main.c
+++ srcs/main.c
@@ -6,36 +6,44 @@
 /*   By: bchaleil <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/07 15:18:58 by bchaleil          #+#    #+#             */
-/*   Updated: 2016/03/15 19:28:26 by bchaleil         ###   ########.fr       */
+/*   Updated: 2016/03/16 21:42:07 by bchaleil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
 
+void printList(t_file *node)
+{
+	while (node != NULL)
+	{
+		ft_putendl(node->name);
+		node = node->next;
+	}
+}
+
 int	main(int ac, char **av)
 {
 
 	t_file	*f;
-	/*
 	t_flag	flag;
-	flag = argv_check(ac, av);
+	char	**files;
+//	char	*result;
+	int		i;
+
+	i = 0;
+	flag = options_check(ac, av);
 	//debug
 	ft_putstr("flags: ");
 	ft_print_binary(flag);
 	ft_putstr("\n");
-	*/
-	if (ac == 1)
-		return (0);
-	f = list_file_info(av[1]);
-	while (f)
+	files = files_check(ac, av); 
+	while (files[i])
 	{
-		ft_putstr("st mode ");
-		print_mode(f->fs.st_mode);
-		ft_putstr("\nf->name ");
-		ft_putendl(f->name);
-		ft_putstr("f->path ");
-		ft_putendl(f->path);
-		f = f->next;
+		f = get_t_file_info(files[i]);
+		merge_sort(&f, order);
+		format_file(f, flag);
+		ft_putendl(files[i]);
+		i++;
 	}
 	return (0);
 }
