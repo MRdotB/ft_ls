54e56ee52e5dbf463c1b9d071971ce3a948f5315
Baptiste CHALEIL <bchaleil@e3r9p5.42.fr>
2016-03-16 13:50:57 +0100

    merge sort

---
+++ srcs/merge_sort.c
@@ -0,0 +1,80 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   merge_sort.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bchaleil <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/03/16 11:16:23 by bchaleil          #+#    #+#             */
+/*   Updated: 2016/03/16 13:44:10 by bchaleil         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_ls.h"
+
+static t_file	*sorted_merge(t_file* a, t_file* b, int (*f)(t_file*, t_file*))
+{
+	t_file* result = NULL;
+
+	if (a == NULL)
+		return (b);
+	else if (b == NULL)
+		return (a);
+
+	if (f(a, b))
+	{
+		result = a;
+		result->next = sorted_merge(a->next, b, f);
+	}
+	else
+	{
+		result = b;
+		result->next = sorted_merge(a, b->next, f);
+	}
+	return (result);
+}
+
+static void		front_back_split(t_file* source, t_file** front_ref,
+		t_file** back_ref)
+{
+	t_file* slow;
+	t_file* fast;
+
+	if (source == NULL || source->next == NULL)
+	{
+		*front_ref = source;
+		*back_ref = NULL;
+	}
+	else
+	{
+		slow = source;
+		fast = source->next;
+		while (fast != NULL)
+		{
+			fast = fast->next;
+			if (fast != NULL)
+			{
+				slow = slow->next;
+				fast = fast->next;
+			}
+		}
+		*front_ref = source;
+		*back_ref = slow->next;
+		slow->next = NULL;
+	}
+}
+
+void			merge_sort(t_file** head_ref, int (*f)(t_file*, t_file*))
+{
+	t_file* head;
+	t_file* a;
+	t_file* b;
+
+	head = *head_ref;
+	if ((head == NULL) || (head->next == NULL))
+		return ;
+	front_back_split(head, &a, &b); 
+	merge_sort(&a, f);
+	merge_sort(&b, f);
+	*head_ref = sorted_merge(a, b, f);
+}
